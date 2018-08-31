78d601e3fe988a9de545fad9441c15e955e7702b
Jose Suarez <jose95sp@outlook.com>
2018-08-30 22:07:27 -0500

    added ClientModel

--- src/main/java/com/josespx/confections/model/Client.java
+++ src/main/java/com/josespx/confections/model/Client.java
@@ -1,20 +1,89 @@
 package com.josespx.confections.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "client")
 public class Client {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name", length = 255)
     private String name;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "dni")
     private String dni;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "phone1")
     private String phone1;
+
+    @Column(name = "phone2")
     private String phone2;
 
+    public Client(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
 }
