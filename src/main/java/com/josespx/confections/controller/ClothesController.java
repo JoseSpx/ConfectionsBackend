7352f136521554b500239a49332146945841d25f
Jose Suarez <jose95sp@outlook.com>
2018-09-01 01:03:19 -0500

    added clothes controller

---
+++ src/main/java/com/josespx/confections/controller/ClothesController.java
@@ -0,0 +1,77 @@
+package com.josespx.confections.controller;
+
+import com.josespx.confections.model.Clothes;
+import com.josespx.confections.service.ClothesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping(value = "/api")
+public class ClothesController {
+
+    private ClothesService clothesService;
+
+    @Autowired
+    public ClothesController(ClothesService clothesService) {
+        this.clothesService = clothesService;
+    }
+
+    @RequestMapping(value = "/clothes", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity<Clothes> saveClothes(@RequestBody Clothes clothes, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        this.clothesService.save(clothes);
+        return new ResponseEntity<>(clothes, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/clothes/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<Clothes> findById(@PathVariable("id") Long id) {
+        Clothes clothes = this.clothesService.findById(id);
+        if (clothes == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(clothes, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/clothes", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<List<Clothes>> findAll() {
+        List<Clothes> clothesList = this.clothesService.findAll();
+        if (clothesList == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(clothesList, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/clothes/{id}", method = RequestMethod.PATCH, headers = "Accept=application/json")
+    public ResponseEntity<Clothes> updateClient(@PathVariable("id") Long id, @RequestBody Clothes clothes) {
+        Clothes clothesToUpdate = this.clothesService.findById(id);
+        if (clothesToUpdate == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        clothesToUpdate.setName(clothes.getName());
+        this.clothesService.save(clothesToUpdate);
+        return new ResponseEntity<>(clothesToUpdate, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/clothes/{id}")
+    public ResponseEntity<Clothes> deleteById(@PathVariable("id") Long id) {
+        Clothes clothes = this.clothesService.findById(id);
+        if (clothes == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        this.clothesService.deleteById(id);
+        return new ResponseEntity<>(clothes, HttpStatus.OK);
+    }
+
+}
